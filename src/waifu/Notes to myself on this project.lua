6b4461f3870737a8be4543b476c84a7eae37d060
Amina-V <AminahCVinson@gmail.com>
2025-02-18 18:53:26 -0800

    + Added notes to chat with myself and plan the end of the project.

---
+++ src/waifu/Notes to myself on this project.lua
@@ -0,0 +1,18 @@
+Notes to myself on this project 
+
+What I will add this week:
+[] Lint formatting for the code.
+[] Waifu will have some ASCII art integration
+[] Waifu will have a game integrated (guess the blank thing)
+[] Waifu will have a story mode integrated 
+[] Waifu will have a timer integrated
+[] Waifu will have a write up for the project to be open source ready!
+[] Waifu will have a discord bot integrated 
+[] Waifu will have some social integration(twitter, git, etc)
+[] Kawaii code review?
+[] Update the UI for speaking chat bubbles for waifu once her name is changed.
+
+What works now (18-Feb-2025)
+[x] Waifu will talk to you in the chat 
+[x] Waifu can do Leet code challenges 
+[x] Waifu has a cute UI
